f181fe13c6223033e5366be9e69f3676de15dbbb
ozmansolomon <osman99ahmed@gmail.com>
2021-05-10 10:56:31 +0400

    prechaceImage

--- lib/core/providers/splashProvider.dart
+++ lib/core/providers/splashProvider.dart
@@ -27,19 +27,7 @@ class SplashProvider extends ChangeNotifier {
   }
 
   loadImages(context) async {
-    await precacheImage(AssetImage('assets/images/duration.png'), context);
-    await precacheImage(AssetImage('assets/images/addImg.png'), context);
-    await precacheImage(AssetImage('assets/images/addOffer.png'), context);
     await precacheImage(
         AssetImage('assets/images/addOfferBanner.png'), context);
-    await precacheImage(AssetImage('assets/images/apple.png'), context);
-    await precacheImage(AssetImage('assets/images/background.png'), context);
-    await precacheImage(AssetImage('assets/images/basket.png'), context);
-    await precacheImage(AssetImage('assets/images/budget.png'), context);
-    await precacheImage(AssetImage('assets/images/icon.png'), context);
-    await precacheImage(AssetImage('assets/images/location.png'), context);
-    await precacheImage(AssetImage('assets/images/logo_text.png'), context);
-    await precacheImage(AssetImage('assets/images/logo_white.png'), context);
-    await precacheImage(AssetImage('assets/images/logo.png'), context);
   }
 }
